bd06aa0902cd366d39fcf850185bab9b9aecbd39
wohara224 <wohara@youtheng.co.jp>
2026-06-05 18:25:28 +0900

    Client

---
+++ GradeJudge.Client/Model/Failds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Program;
+
+namespace GradeJudge.Client.Model
+{
+    // 補習者リスト
+    public class Failds
+    {
+        public List<StudentGrade> Faild { get; set; }
+    }
+}
