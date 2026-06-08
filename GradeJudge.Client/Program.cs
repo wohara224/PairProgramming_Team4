989bb1762dffa21bade883a37a6e4570d2273eeb
wohara224 <wohara@youtheng.co.jp>
2026-06-08 11:58:40 +0900

    Update Client

--- GradeJudge.Client/Program.cs
+++ GradeJudge.Client/Program.cs
@@ -1,17 +1,24 @@
-﻿using System.Net.Http.Json;
+﻿using GradeJudge.Client.Model;
+using NLog;
+using System.Net.Http.Json;
 using System.Numerics;
 using System.Text;
 using System.Text.Json;
 using System.Xml.Linq;
-using GradeJudge.Client.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-//using NLog.Web;
 
-public class Program
+
+public partial class Program
 {
     static HttpClient client = new();
-    //static string deviceUrl = "http://172.16.7.10:50080/"; // サーバーURL
-    static string deviceUrl = "http://localhost:50080/";
+    // static string deviceUrl = "http://172.16.7.10:8080"; // サーバーURL
+    static string deviceUrl = "http://localhost:8080";
+
+    // ロガー設定
+    private static readonly Logger sysLogger = LogManager.GetLogger("GradeJudge.Client.System"); // メイン操作用
+    private static readonly Logger apiLogger = LogManager.GetLogger("GradeJudge.Client.Api"); // 通信ログ
+
 
     // 画面の列挙
     enum Grades
@@ -26,6 +33,9 @@ public class Program
     // メインループ
     static async Task Main()
     {
+        // システムログ：アプリ起動 
+        sysLogger.Info("アプリケーション起動");
+
         Grades state = Grades.Title;
         
         while (state != Grades.Exit)
@@ -51,14 +61,21 @@ public class Program
 
             }
         }
+
+        Console.WriteLine("");
+        Console.WriteLine("アプリケーションを終了します．．．");
+        sysLogger.Info("アプリケーション終了");
     }
 
     //　初期画面
     static async Task<Grades> TitleScene()
     {
+        // システムログ：初期画面
+        sysLogger.Info("初期画面表示");
+
         Console.Clear();
 
-        Console.WriteLine("0：終了 1：閲覧 2：登録");
+        Console.WriteLine("0：終了 1：登録 2：個人成績閲覧 3：科目別ランキング");
         Console.Write("操作入力＞");
         string? command = Console.ReadLine();
 
@@ -67,9 +84,9 @@ public class Program
         {
             case "0":
                 return Grades.Exit;
-            case "1":
-                return Grades.ViewGrades;
             case "2":
+                return Grades.ViewGrades;
+            case "1":
                 return Grades.AddGrades;
             case "3":
                 return Grades.RankingGrades;
@@ -81,6 +98,9 @@ public class Program
     // 成績閲覧画面
     static async Task<Grades> ViewScene()
     {
+        // システムログ：成績閲覧画面
+        sysLogger.Info("個人成績閲覧画面表示");
+
         Console.Clear();
         while (true)
         {
@@ -94,53 +114,83 @@ public class Program
                 return Grades.Title;
             }
 
-            // API通信と表示
-            var response = await client.GetAsync($"{deviceUrl}/api/performance?student={studentId}");
-
             Console.SetCursorPosition(0, 1);
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Console.WriteLine(new string(' ', Console.WindowWidth));
             }
             Console.SetCursorPosition(0, 3);
 
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var student = await response.Content.ReadFromJsonAsync<StudentGrade>();
+                // API通信と表示
+                apiLogger.Info("GET送信:URL={0}/api/scores/student?id={1}", deviceUrl, studentId);
+                var response = await client.GetAsync($"{deviceUrl}/api/scores/student?id={studentId}");
 
-                Console.WriteLine($"名前：{student.Name,5}");
-                Console.WriteLine("----------------------------------");
-                foreach (var subject in student.Subjects)
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine($"{subject.Name,5}:{subject.Score,4}点");
+                    var student = await response.Content.ReadFromJsonAsync<StudentGrade>();
+
+                    apiLogger.Info("正常レスポンス:API={0}", student);
+
+                    Console.WriteLine($"名前：{student.Name,5}");
+                    Console.WriteLine("----------------------------------");
+                    foreach (var subject in student.Subjects)
+                    {
+                        Console.WriteLine($"{subject.Name,5}:{subject.Score,4}点");
+                    }
+                }
+                else
+                {
+                    var error =  await response.Content.ReadFromJsonAsync<List<ApiError>>();
+
+                    apiLogger.Warn("異常レスポンス:API={0}", error);
+
+                    foreach (var err in error)
+                    {
+                        switch (err?.Message)
+                        {
+                            case "INVALID_REQUEST":
+                                Console.WriteLine("生徒IDが不正です");
+                                break;
+
+                            case "STUDENT_NOT_FOUND":
+                                Console.WriteLine("生徒が存在しません");
+                                break;
+
+                            default:
+                                Console.WriteLine("アクセス失敗");
+                                break;
+                        }
+                    }
                 }
             }
-            else
+            catch (HttpRequestException ex)
             {
-                var error = await response.Content.ReadFromJsonAsync<ApiError>();
-
-                if (error?.Code == "INVALID_STUDENT_ID")
-                {
-                    Console.WriteLine("生徒IDが不正です");
-                }
-                else if(error?.Code == "STUDENT_NOT_FOUND")
-                {
-                    Console.WriteLine("生徒が存在しません");
-                }
-                else if (error?.Code == "NO_PERMISSION")
-                {
-                    Console.WriteLine("アクセス失敗");
-                }
+                apiLogger.Warn("通信エラー:{0}", ex.Message);
+                Console.WriteLine($"通信エラー: {ex.Message}");
+            }
+            catch (TaskCanceledException)
+            {
+                apiLogger.Warn("タイムアウト}");
+                Console.WriteLine("タイムアウトしました");
+            }
+            catch (Exception ex)
+            {
+                apiLogger.Warn("予期しないエラー:{0}", ex.Message);
+                Console.WriteLine($"予期しないエラー: {ex.Message}");
             }
 
             Console.SetCursorPosition(0, 0);
         }
-
     }
 
     // 成績追加画面
     static async Task<Grades> AddScene()
     {
+        // システムログ：成績追加画面
+        sysLogger.Info("成績追加画面表示");
+
         Console.Clear();
         while (true)
         {
@@ -158,62 +208,90 @@ public class Program
             string? subjectId = Input();
             if (subjectId == null)
             {
-                return Grades.ViewGrades;
+                return Grades.Title;
             }
 
             Console.Write("点数＞");
             string? score = Input();
             if (score == null)
             {
-                return Grades.ViewGrades;
+                return Grades.Title;
             }
 
-            Console.SetCursorPosition(0, 0);
-            for (int i = 0; i < 10; i++)
+            Console.SetCursorPosition(0, 1);
+            for (int i = 0; i < 20; i++)
             {
                 Console.WriteLine(new string(' ', Console.WindowWidth));
             }
             Console.SetCursorPosition(0, 6);
 
-            // API通信と表示
-
-            var request = new AddData
+            try
             {
-                StudentId = int.Parse(studentId),
-                SubjectId = int.Parse(subjectId),
-                Score = int.Parse(score)
-            };
-            
-            var response = await client.PostAsJsonAsync($"{deviceUrl}/api/register", request);
+                // API通信と表示
 
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("成績の登録に成功しました");
+                var request = new AddData
+                {
+                    StudentId = int.Parse(studentId),
+                    SubjectId = int.Parse(subjectId),
+                    Score = int.Parse(score)
+                };
+
+                apiLogger.Info("POST送信:URL={0}/api/scores/register StudentId={1},SubjectId={2},Score={3}", deviceUrl, request.StudentId,request.SubjectId,request.Score);
+                var response = await client.PostAsJsonAsync($"{deviceUrl}/api/register", request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    apiLogger.Info("正常レスポンス:API={0}", response);
+
+                    Console.WriteLine("成績の登録に成功しました");
+                }
+                else
+                {
+                    var error = await response.Content.ReadFromJsonAsync<List<ApiError>>();
+
+                    apiLogger.Warn("異常レスポンス:API={0}", error);
+
+                    foreach (var err in error)
+                    {
+                        switch (err?.Message)
+                        {
+                            case "INVALID_REQUEST":
+                                Console.WriteLine("リクエストが不正です");
+                                break;
+
+                            case "STUDENT_NOT_EXIST":
+                                Console.WriteLine("生徒が存在しません");
+                                break;
+
+                            case "SUBJECT_NOT_EXIST":
+                                Console.WriteLine("科目が存在しません");
+                                break;
+
+                            case "SCORE_OUT_OF_RANGE":
+                                Console.WriteLine("点数が範囲外です");
+                                break;
+
+                            default:
+                                Console.WriteLine("アクセス失敗");
+                                break;
+                        }
+                    }
+                }
             }
-            else
+            catch (HttpRequestException ex)
             {
-                var error = await response.Content.ReadFromJsonAsync<ApiError>();
-
-                if (error?.Code == "INVALID_REQUEST")
-                {
-                    Console.WriteLine("リクエストが不正です");
-                }
-                else if (error?.Code == "STUDENT_NOT_EXIST")
-                {
-                    Console.WriteLine("生徒が存在しません");
-                }
-                else if (error?.Code == "SUBJECT_NOT_EXIST")
-                {
-                    Console.WriteLine("科目が存在しません");
-                }
-                else if (error?.Code == "SCORE_OUT_OF_RANGE")
-                {
-                    Console.WriteLine("点数が範囲外です");
-                }
-                else if (error?.Code == "NO_PERMISSION")
-                {
-                    Console.WriteLine("アクセス失敗");
-                }
+                apiLogger.Warn("通信エラー:{0}", ex.Message);
+                Console.WriteLine($"通信エラー: {ex.Message}");
+            }
+            catch (TaskCanceledException)
+            {
+                apiLogger.Warn("タイムアウト}");
+                Console.WriteLine("タイムアウトしました");
+            }
+            catch (Exception ex)
+            {
+                apiLogger.Warn("予期しないエラー:{0}", ex.Message);
+                Console.WriteLine($"予期しないエラー: {ex.Message}");
             }
 
             Console.SetCursorPosition(0, 0);
@@ -223,6 +301,9 @@ public class Program
     // 成績ランキング画面
     static async Task<Grades> RankingScene()
     {
+        // システムログ：成績ランキング画面
+        sysLogger.Info("ランキング画面表示");
+
         Console.Clear();
         while (true)
         {
@@ -236,43 +317,74 @@ public class Program
                 return Grades.Title;
             }
 
-            // API通信と表示
-            var response = await client.GetAsync($"{deviceUrl}/api/ranking?subject={subjectId}");
-
             Console.SetCursorPosition(0, 1);
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Console.WriteLine(new string(' ', Console.WindowWidth));
             }
             Console.SetCursorPosition(0, 3);
 
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var subject = await response.Content.ReadFromJsonAsync<SubjectGrade>();
+                // API通信と表示
+                apiLogger.Info("GET送信:URL={0}/api/scores/subject?id={1}", deviceUrl, subjectId);
+                var response = await client.GetAsync($"{deviceUrl}/api/scores/subject?id={subjectId}");
 
-                Console.WriteLine($"名前：{subject.Name,5}");
-                Console.WriteLine("----------------------------------");
-                foreach (var student in subject.Students)
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine($"{student.Name,5}:{student.Score,4}点");
+                    var subject = await response.Content.ReadFromJsonAsync<SubjectGrade>();
+
+                    apiLogger.Info("正常レスポンス:API={0}", subject);
+
+                    var top5 = subject.Students.OrderByDescending(s => s.Score).Take(5).ToList();
+
+
+                    Console.WriteLine($"名前：{subject.Name,5}");
+                    Console.WriteLine("----------------------------------");
+                    foreach (var student in top5)
+                    {
+                        Console.WriteLine($"{student.Name,5}:{student.Score,4}点");
+                    }
+                }
+                else
+                {
+                    var error = await response.Content.ReadFromJsonAsync<List<ApiError>>();
+
+                    apiLogger.Warn("異常レスポンス:API={0}", error);
+
+                    foreach (var err in error)
+                    {
+                        switch (err?.Message)
+                        {
+                            case "INVALID_REQUEST":
+                                Console.WriteLine("科目IDが不正です");
+                                break;
+
+                            case "SUBJECT_NOT_FOUND":
+                                Console.WriteLine("科目が存在しません");
+                                break;
+
+                            default:
+                                Console.WriteLine("アクセス失敗");
+                                break;
+                        }
+                    }
                 }
             }
-            else
+            catch (HttpRequestException ex)
             {
-                var error = await response.Content.ReadFromJsonAsync<ApiError>();
-
-                if (error?.Code == "INVALID_SUBJECT_ID")
-                {
-                    Console.WriteLine("科目IDが不正です");
-                }
-                else if (error?.Code == "SUBJECT_NOT_FOUND")
-                {
-                    Console.WriteLine("科目が存在しません");
-                }
-                else if (error?.Code == "NO_PERMISSION")
-                {
-                    Console.WriteLine("アクセス失敗");
-                }
+                apiLogger.Warn("通信エラー:{0}", ex.Message);
+                Console.WriteLine($"通信エラー: {ex.Message}");
+            }
+            catch (TaskCanceledException)
+            {
+                apiLogger.Warn("タイムアウト}");
+                Console.WriteLine("タイムアウトしました");
+            }
+            catch (Exception ex)
+            {
+                apiLogger.Warn("予期しないエラー:{0}", ex.Message);
+                Console.WriteLine($"予期しないエラー: {ex.Message}");
             }
 
             Console.SetCursorPosition(0, 0);
